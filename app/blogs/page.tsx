19169d3cff177b996461287ed409034c6adced2c
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2026-01-06 22:55:26 +0530

    update: sanity configs

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,11 +1,11 @@
 import { client } from "@/sanity/lib/client";
 import BlogList from "./BlogList";
 
-// Revalidate every hour
-export const revalidate = 3600;
+// Revalidate every 60 seconds (1 minute)
+export const revalidate = 60;
 
 async function getPosts() {
-    return await client.fetch(`
+  return await client.fetch(`
     *[_type == "post"] | order(publishedAt desc) {
       _id,
       title,
@@ -21,11 +21,11 @@ async function getPosts() {
 }
 
 export default async function BlogPage() {
-    const posts = await getPosts();
+  const posts = await getPosts();
 
-    return (
-        <div className="min-h-screen pt-24 pb-20 bg-background">
-            <BlogList posts={posts} />
-        </div>
-    );
+  return (
+    <div className="min-h-screen pt-24 pb-20 bg-background">
+      <BlogList posts={posts} />
+    </div>
+  );
 }
