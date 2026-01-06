df94cb7e7d1a0c0698be268fab626e45c1c0adbd
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2026-01-06 22:35:52 +0530

    update: added a temporary OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,12 +29,21 @@ export const metadata: Metadata = {
     siteName: "Silicon M-ulation",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "/assets/default-og.png",
+        width: 1200,
+        height: 630,
+        alt: "Silicon M-ulation",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Silicon M-ulation",
     description: "Mac Gaming & Emulation Reference for Apple Silicon.",
     creator: "@AgileCoderYT",
+    images: ["/assets/default-og.png"],
   },
 };
 
